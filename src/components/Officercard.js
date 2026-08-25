2246090c017e52a782941d657a4beb87dbd30e27
purduefencingclub <purduefencingclub@gmail.com>
2026-08-25 19:31:21 -0400

    officercard layout

--- src/components/Officercard.js
+++ src/components/Officercard.js
@@ -15,7 +15,7 @@ const Officercard = ({name, imagesrc, position, major, bladeimg, blade, year, ye
                 <Col xs={7}>
                 <p className="officer-info">{major}</p>
                 </Col>
-                <Col xs={2}>
+                <Col xs={3}>
                 <img src={bladeimg} width="45%" />
                 <p className="officer-info">{blade}</p>
                 </Col>
